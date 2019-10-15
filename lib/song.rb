66ae070db2b020fec3738972ab3a04ea5d123135
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-15 13:49:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,11 +4,13 @@ class Song
   
   @@count = 0
   
+  
  def initialize(name, artist, genre) 
    @name = name
    @artist = artist
    @genre = genre
    @@count += 1
+   @@artist << artist
  end 
  
  def self.count
