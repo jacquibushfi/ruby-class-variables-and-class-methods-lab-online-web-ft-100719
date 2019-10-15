c499de585d36ce7d978ba0691b33b74a9f9c5135
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-15 13:52:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,14 +3,16 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
-  
+  @@genres = []
   
  def initialize(name, artist, genre) 
    @name = name
    @artist = artist
    @genre = genre
    @@count += 1
-   @@artist << artist
+   
+   
+   
  end 
  
  def self.count
