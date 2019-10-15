0ff95f51d40b825af65a852294bb38c719e70b91
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-15 14:16:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,15 +48,15 @@ class Song
 	
 	def self.artist_count
    artist_count = {}
-	 @@genres.each do |genre|
-	    if genre_count[genre] 
-			   genre_count[genre] += 1
+	 @@artists.each do |artist|
+	    if artist_count[artist] 
+			   artist_count[artist] += 1
 			else
-			   genre_count[genre] = 1
+			   artist_count[artist] = 1
 			end
 			
 	end
-	genre_count
+	artist_count
 	end
  
 end
